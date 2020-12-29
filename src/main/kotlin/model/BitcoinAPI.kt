e3eefae4feefa0e926eade69fe1a4b03623ed036
Mohammed Ali <mohammed.ali@students.bfh.ch>
2020-12-29 17:57:04 +0100

    updatedTime label is updated at runtime using Coroutines v1

--- src/main/kotlin/model/BitcoinAPI.kt
+++ src/main/kotlin/model/BitcoinAPI.kt
@@ -11,7 +11,6 @@ import org.json.JSONObject
 
 class BitcoinAPI {
 
-    private var connection = Connection()
     private var responseContent: StringBuffer? = null
     private var updatedTime: String? = null
 
@@ -27,30 +26,41 @@ class BitcoinAPI {
     }
 
 
-    fun apiRequestJson() {
-         connection.connect("https://api.coindesk.com/v1/bpi/currentprice.json")
-         responseContent = connection.responseContent
+
+
+    suspend fun getUpdateTime() {
+        CoroutineScope(IO).launch {
+            while (true) {
+                val jsonObject = getAPIdata()
+                val node = parse(jsonObject.getJSONObject("time").toString())
+                updatedTime = node.get("updated").asText()
+                println("in get update time " + updatedTime)
+                setUpdateTime(node.get("updated").asText())
+                delay(3000)
+            }
+        }
+
+        //return jsonObject.getJSONObject("time").getString("updated")
+        //return jsonObject.getJSONObject("bpi").getJSONObject("USD").getString("description")
+    }
+
+    suspend fun getUpdateT(): String{
+        getUpdateTime()
+        return this.updatedTime.toString()
 
     }
 
 
-    suspend fun getUpdateTime(): String {
-        CoroutineScope(IO).launch {
-            println("in get update time")
-            while (true) {
-                apiRequestJson()
-                val jsonObject = JSONObject(responseContent.toString())
-                var node = parse(jsonObject.getJSONObject("time").toString())
-                updatedTime = node.get("updated").asText()
-                println(updatedTime)
-                connection = Connection()
-                delay(6000)
-            }
-        }
-        return this.updatedTime.toString()
+    fun setUpdateTime(str:String?){
+        this.updatedTime = str
+    }
 
-        //return jsonObject.getJSONObject("time").getString("updated")
-        //return jsonObject.getJSONObject("bpi").getJSONObject("USD").getString("description")
+
+    fun getAPIdata(): JSONObject {
+        val connection = Connection()
+        connection.connect("https://api.coindesk.com/v1/bpi/currentprice.json")
+        responseContent = connection.responseContent
+        return JSONObject(responseContent.toString())
     }
 
 }
--- src/main/kotlin/view/MainView.kt
+++ src/main/kotlin/view/MainView.kt
@@ -1,7 +1,13 @@
 
 package view
 
+import javafx.beans.property.SimpleStringProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import model.Styles
 import model.BitcoinAPI
@@ -9,11 +15,20 @@ import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
     override val root = hbox {
-        var test = BitcoinAPI()
-        runBlocking {
-            label(test.getUpdateTime()) {
-                addClass(Styles.heading)
+        var some = SimpleStringProperty()
+        some.value = "hallo"
+
+        val bitcoinAPI = BitcoinAPI()
+        CoroutineScope(Main).launch {
+            while (true) {
+                some.value = bitcoinAPI.getUpdateT()
+                println("in main view" + bitcoinAPI.getUpdateT())
+                delay(5000)
             }
+        }
+        label {
+            bind(some)
+            addClass(Styles.heading)
         }
 
     }
